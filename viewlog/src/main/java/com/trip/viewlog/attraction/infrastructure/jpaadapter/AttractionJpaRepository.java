18e1ce9a96b2c436a96be4658622c9d645e48202
Spinichi <spinichi@naver.com>
2025-05-17 22:46:03 +0900

    feat[#2]: attractionRepository 구현

---
+++ viewlog/src/main/java/com/trip/viewlog/attraction/infrastructure/jpaadapter/AttractionJpaRepository.java
@@ -0,0 +1,19 @@
+package com.trip.viewlog.attraction.infrastructure.jpaadapter;
+
+import com.trip.viewlog.attraction.infrastructure.jpaadapter.entity.AttractionEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AttractionJpaRepository extends JpaRepository<AttractionEntity, Integer> {
+
+	@EntityGraph(attributePaths = "contentTypeEntity")
+	List<AttractionEntity> findByLatitudeBetweenAndLongitudeBetween(double minLat, double maxLat, double minLng, double maxLng);
+	
+	@EntityGraph(attributePaths = "contentTypeEntity")
+	List<AttractionEntity> findByAddr1ContainingOrAddr2Containing(String addr1Keyword, String addr2Keyword);
+	
+	@EntityGraph(attributePaths = "contentTypeEntity")
+	List<AttractionEntity> findByTitleContaining(String keyword);
+}
