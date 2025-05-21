1692c2f1de255cbae93c70ebfb188b6098152522
Spinichi <spinichi@naver.com>
2025-05-21 16:21:49 +0900

    feat: 권한 경로 수정

--- viewlog/src/main/java/com/trip/viewlog/global/config/security/SecurityConfig.java
+++ viewlog/src/main/java/com/trip/viewlog/global/config/security/SecurityConfig.java
@@ -80,7 +80,6 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers(
-                                "/",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
