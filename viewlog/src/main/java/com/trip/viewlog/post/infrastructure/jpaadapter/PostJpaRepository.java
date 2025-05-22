4ff959acacf55bddc9170d9dcc9ac069b190ee62
Spinichi <spinichi@naver.com>
2025-05-22 15:18:59 +0900

    feat: í페이징 및 게시글 작성 메서드 추가

--- viewlog/src/main/java/com/trip/viewlog/post/infrastructure/jpaadapter/PostJpaRepository.java
+++ viewlog/src/main/java/com/trip/viewlog/post/infrastructure/jpaadapter/PostJpaRepository.java
@@ -3,7 +3,5 @@ package com.trip.viewlog.post.infrastructure.jpaadapter;
 import com.trip.viewlog.post.infrastructure.jpaadapter.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PostJpaRepository extends JpaRepository<PostEntity, Long> {
 }
