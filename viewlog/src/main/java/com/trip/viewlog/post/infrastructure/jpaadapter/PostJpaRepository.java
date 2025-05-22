95b5989bcb71bc20bda67272f272c56be4f7a650
Spinichi <spinichi@naver.com>
2025-05-22 20:01:23 +0900

    refactor: 쿼리 최적화를 위환 EntityGraph 추가

--- viewlog/src/main/java/com/trip/viewlog/post/infrastructure/jpaadapter/PostJpaRepository.java
+++ viewlog/src/main/java/com/trip/viewlog/post/infrastructure/jpaadapter/PostJpaRepository.java
@@ -1,8 +1,14 @@
 package com.trip.viewlog.post.infrastructure.jpaadapter;
 
+import com.trip.viewlog.post.domain.Post;
 import com.trip.viewlog.post.infrastructure.jpaadapter.entity.PostEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface PostJpaRepository extends JpaRepository<PostEntity, Long> , PagingAndSortingRepository<PostEntity, Long> {
+    @EntityGraph(attributePaths = {"userEntity"})
+    Page<PostEntity> findAll(Pageable pageable);
 }
