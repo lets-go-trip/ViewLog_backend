85cb34f9084f9801b969fae82e428d626da43219
Spinichi <spinichi@naver.com>
2025-05-22 15:16:12 +0900

    chore: 으ëì의미없는 ãjpa 메쿼리 메서드 제거

--- viewlog/src/main/java/com/trip/viewlog/post/infrastructure/jpaadapter/PostJpaRepository.java
+++ viewlog/src/main/java/com/trip/viewlog/post/infrastructure/jpaadapter/PostJpaRepository.java
@@ -6,7 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PostJpaRepository extends JpaRepository<PostEntity, Long> {
-	List<PostEntity> findAllByOrderByCreatedAtDesc();
-	
-	
 }
