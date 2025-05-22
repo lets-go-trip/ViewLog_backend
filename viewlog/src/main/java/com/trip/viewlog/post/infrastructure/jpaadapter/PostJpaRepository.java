73e42422f3d6c74f496cc9005aeaf81fb3774f31
Spinichi <spinichi@naver.com>
2025-05-22 15:21:29 +0900

    feat: 페이징 및 게시글 저장 구현

--- viewlog/src/main/java/com/trip/viewlog/post/infrastructure/jpaadapter/PostJpaRepository.java
+++ viewlog/src/main/java/com/trip/viewlog/post/infrastructure/jpaadapter/PostJpaRepository.java
@@ -2,6 +2,7 @@ package com.trip.viewlog.post.infrastructure.jpaadapter;
 
 import com.trip.viewlog.post.infrastructure.jpaadapter.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface PostJpaRepository extends JpaRepository<PostEntity, Long> {
+public interface PostJpaRepository extends JpaRepository<PostEntity, Long> , PagingAndSortingRepository<PostEntity, Long> {
 }
--- viewlog/src/main/java/com/trip/viewlog/post/infrastructure/jpaadapter/PostRepositoryImpl.java
+++ viewlog/src/main/java/com/trip/viewlog/post/infrastructure/jpaadapter/PostRepositoryImpl.java
@@ -4,28 +4,37 @@ import com.trip.viewlog.post.application.outputport.PostRepository;
 import com.trip.viewlog.post.domain.Post;
 import com.trip.viewlog.post.infrastructure.jpaadapter.entity.PostEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class PostRepositoryImpl implements PostRepository{
-	
+
 	private final PostJpaRepository postJpaRepository;
-	
+
 	@Override
-	public List<Post> findAllByOrderByCreatedAtDesc() {
-		return postJpaRepository.findAllByOrderByCreatedAtDesc()
-				.stream()
-				.map(PostEntity::toModel)
-				.toList();
+	public Page<Post> findAll(Pageable pageable) {
+		return postJpaRepository.findAll(pageable)
+				.map(PostEntity::toModel);
 	}
-	
+
 	@Override
 	public Optional<Post> findById(Long id) {
 		return postJpaRepository.findById(id).map(PostEntity::toModel);
+	}
+
+	@Override
+	public Post save(Post post) {
+		// 도메인 → 엔티티
+		PostEntity entity = PostEntity.from(post);
+		// JPA 저장
+		PostEntity saved = postJpaRepository.save(entity);
+		// 엔티티 → 도메인
+		return saved.toModel();
 	}
 
 }
